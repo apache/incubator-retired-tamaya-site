21b98714c4b45c94126a5ed61333f2ead9a3b21c
Phil Ottlinger <pottlinger@apache.org>
2016-10-24 22:19:05 +0200

    TAMAYA-178: New site draft with link to API/Javadoc.

--- copy-site.sh
+++ copy-site.sh
@@ -1,5 +1,5 @@
  #!/bin/bash          
 echo "Copying generated site into current branch ..."
-cp -r ./original/* .
+cp -r ./output/* .
 echo "Do not forget to copy apidocs into ./apidocs before pushing."
 echo "DONE"
