7c72ce19e8ea09dc1cc51168e49d589082fc031c
Phil Ottlinger <pottlinger@apache.org>
2017-01-26 23:11:52 +0100

    Add reset to jenkins shell script
    * It's not really clear why the build gets stuck; works locally in 8s

--- bake-jenkins.sh
+++ bake-jenkins.sh
@@ -2,6 +2,6 @@
 echo "Starting to bake our project homepage ...."
 export JBAKE_HOME="$(dirname "$0")/bin/jbake-2.5.0"
 echo $JBAKE_HOME
-$JBAKE_HOME/bin/jbake -b
+$JBAKE_HOME/bin/jbake -b --reset
 echo "DONE"
 
