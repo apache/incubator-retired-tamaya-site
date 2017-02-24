c87abab527bcd4cf46c86ced3d13a17c96bb441d
Phil Ottlinger <pottlinger@apache.org>
2017-02-24 22:10:52 +0100

    TAMAYA-237: Use new version

--- copysite.sh
+++ copysite.sh
@@ -1,6 +1,6 @@
  #!/bin/bash          
 echo "Starting to bake our project homepage ...."
-export JBAKE_HOME="$(dirname "$0")/bin/jbake-2.5.0"
+export JBAKE_HOME="$(dirname "$0")/bin/jbake-2.5.1"
 echo $JBAKE_HOME
 $JBAKE_HOME/bin/jbake -b
 echo "Copying stuff from original into new branch ..."
