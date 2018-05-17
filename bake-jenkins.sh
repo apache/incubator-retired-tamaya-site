4a42159ad2d21c13826137c527d54abbbd62bfeb
Phil Ottlinger <pottlinger@apache.org>
2018-05-17 23:26:28 +0200

    TAMAYA-338: Fix CI/local build scripts to use new version

--- bake-jenkins.sh
+++ bake-jenkins.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -x
 echo "Starting to bake our project homepage ...."
-export JBAKE_HOME="$(dirname "$0")/bin/jbake-2.5.1"
+export JBAKE_HOME="$(dirname "$0")/bin/jbake-2.6.1-bin"
 echo $JBAKE_HOME
 $JBAKE_HOME/bin/jbake -b --reset
 echo "DONE"
--- bake.sh
+++ bake.sh
@@ -3,6 +3,6 @@ clear
 echo "Cleaning up cache ..."
 rm -rf cache/
 echo "Starting to bake our project homepage ...."
-export JBAKE_HOME="$(dirname "$0")/bin/jbake-2.5.1"
+export JBAKE_HOME="$(dirname "$0")/bin/jbake-2.6.1-bin"
 echo $JBAKE_HOME
 $JBAKE_HOME/bin/jbake -b -s
