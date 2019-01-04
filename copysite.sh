11c21e44c79a15df9f937340ddc72a8d9de66762
Hugo Hirsch <github@hugo-hirsch.de>
2019-01-04 17:02:05 +0100

    TAMAYA-373: Fix copy script

--- copysite.sh
+++ copysite.sh
@@ -1,6 +1,6 @@
  #!/bin/bash          
 echo "Starting to bake our project homepage ...."
-export JBAKE_HOME="$(dirname "$0")/bin/jbake-2.6.1-bin"
+export JBAKE_HOME="$(dirname "$0")/bin/jbake-2.6.3-bin"
 echo $JBAKE_HOME
 $JBAKE_HOME/bin/jbake -b
 echo "Copying stuff from original into new branch ..."
