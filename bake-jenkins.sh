10293f0f685c03ac338f3f342cc53ccdf9b70746
Hugo Hirsch <github@hugo-hirsch.de>
2019-05-12 23:52:20 +0200

    TAMAYA-393: Update to jbake 2.6.4

--- bake-jenkins.sh
+++ bake-jenkins.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -x
 echo "Starting to bake our project homepage ...."
-export JBAKE_HOME="$(dirname "$0")/bin/jbake-2.6.3-bin"
+export JBAKE_HOME="$(dirname "$0")/bin/jbake-2.6.4-bin"
 echo $JBAKE_HOME
 $JBAKE_HOME/bin/jbake -b --reset
 echo "DONE"
