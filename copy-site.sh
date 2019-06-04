8600224350c4d91133cfbd5fed2eaf71274af631
Hugo Hirsch <github@hugo-hirsch.de>
2019-06-04 21:09:28 +0200

    TAMAYA-392: Pull before pushing new site

--- copy-site.sh
+++ copy-site.sh
@@ -1,4 +1,6 @@
  #!/bin/bash          
+echo "Requesting upstream changes first"
+git pull
 echo "Copying generated site into current branch ..."
 cp -r ./output/* .
 echo "Do not forget to copy apidocs into ./apidocs before pushing."
